afa249b4a5bebeb0d3a28f432d20ed50407997d8
dekapx <xkapil@gmail.com>
2023-10-26 11:25:46 +0100

    credit score service

--- credit-score-service-client/src/main/java/com/dekapx/apps/Application.java
+++ credit-score-service-client/src/main/java/com/dekapx/apps/Application.java
@@ -2,6 +2,7 @@ package com.dekapx.apps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
--- credit-score-service-client/src/main/java/com/dekapx/apps/client/CreditScoreClient.java
+++ credit-score-service-client/src/main/java/com/dekapx/apps/client/CreditScoreClient.java
@@ -1,14 +1,14 @@
 package com.dekapx.apps.client;
 
+import com.dekapx.apps.config.CreditScoreFeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import static com.dekapx.apps.common.ResourceUrls.BASE_URL;
-import static com.dekapx.apps.common.ResourceUrls.CREDIT_SCORE_SERVICE_URL;
 import static com.dekapx.apps.common.ResourceUrls.INFO_URL;
 
-@FeignClient(name = "CreditScoreClient", url = CREDIT_SCORE_SERVICE_URL, path = BASE_URL)
+@FeignClient(name = "creditScoreClient", url = "${feign.client.config.creditScoreClient.url}")
 public interface CreditScoreClient {
     @GetMapping(INFO_URL)
     String getInfo();
 }
+
